b31714abd1da0463c8b58294ee61dabc9615a1b4
diegorezm <dmourarezende@gmail.com>
2024-10-10 18:46:25 -0300

    better deleting

--- src/pages/employee/_card.tsx
+++ src/pages/employee/_card.tsx
@@ -5,6 +5,7 @@ import {useOpenUpdateEmployeeSheet} from "./_use-open-update-employee-sheet";
 import {actions} from "astro:actions";
 import {toast} from "sonner";
 import {useShouldUpdate} from "./_should_update_event";
+import {useState} from "react";
 
 const formatDate = (dateString: string) => {
   const options: Intl.DateTimeFormatOptions = {
@@ -22,17 +23,29 @@ type EmployeeCardProps = {
 const EmployeeCard: React.FC<EmployeeCardProps> = ({employee}) => {
   const {onOpen} = useOpenUpdateEmployeeSheet()
   const {onShouldUpdate} = useShouldUpdate()
+  const [isLoading, setIsLoading] = useState(false)
   const onEdit = (employee: Employee) => {
     onOpen(employee)
   }
 
   const onDelete = async (id: string) => {
-    const {error} = await actions.deleteEmployee({id})
-    if (error) {
-      toast.error(error.message ?? "Failed to delete employee")
+    try {
+      setIsLoading(true)
+      const {error} = await actions.deleteEmployee({id})
+      if (error) {
+        throw error
+      }
+      toast.success("Employee deleted successfully!")
+      onShouldUpdate();
+    } catch (e) {
+      if (e instanceof Error) {
+        toast.error(e.message)
+      } else {
+        toast.error("Failed to delete employee")
+      }
+    } finally {
+      setIsLoading(false)
     }
-    toast.success("Employee deleted successfully!")
-    onShouldUpdate();
   }
 
   return (
@@ -47,7 +60,9 @@ const EmployeeCard: React.FC<EmployeeCardProps> = ({employee}) => {
         <p className="text-sm text-secondary-foreground">{employee.job}</p>
         <div className="mt-4 flex space-x-2 w-full">
           <Button onClick={() => onEdit(employee)} className="w-full lg:w-1/2">Edit</Button>
-          <Button variant="destructive" onClick={() => onDelete(employee.id)} className="w-full lg:w-1/2">Delete</Button>
+          <Button variant="destructive" disabled={isLoading} onClick={() => onDelete(employee.id)} className="w-full lg:w-1/2">
+            {isLoading ? "Deleting..." : "Delete"}
+          </Button>
         </div>
       </CardContent>
     </Card>
